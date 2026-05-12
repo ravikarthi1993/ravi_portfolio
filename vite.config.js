b493ae9e32514310fcb54a87c7d7e798f0bc9b59
ravikarthi1993 <ravikarthi32@gmail.com>
2026-05-12 11:37:28 +0530

    Changed URL

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import babel from "@rolldown/plugin-babel";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-  base: "/ravi_portfolio",
+  // base: "/ravi_portfolio",
+  base: "/",
 });
